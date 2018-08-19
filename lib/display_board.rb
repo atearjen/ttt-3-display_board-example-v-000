007c6ee0af31e18df3bf95d972d00bdcbb75ed2d
Allison Tearjen <amtearjen@ucdavis.edu>
2018-08-19 19:51:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define a method print_board that prints a 3x3 Tic Tac Toe Board
+def print_board
 print("   |   |   ")
 print("-----------")
 print("   |   |   ")
 print("-----------")
 print("   |   |   ")
+
+end
